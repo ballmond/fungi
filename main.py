553ce8814d8f469968b6b050cc03c2c4d8400db2
ballmond <ballmond@gmail.com>
2024-11-09 21:20:24 -0500

    shape key updates

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from fasthtml.common import *
 css = Style(':root {--pico-font-size:90%,--pico-font-family: Pacifico, cursive;}')
 app = FastHTML(hdrs=(picolink, css))
 
+def home():
+   return Table(Tbody(Tr(
+            Td(Div(
+                A("Home", href="/"), 
+                # " | ", 
+                # A("Shape Key", href="/shape-key"), 
+                # " | ", 
+                # A("Species List", href="/species-list"),
+                align="center")),
+            ),),width="100%", border="0",),
 
 @app.route("/")
 def get():
     return (Title("Wood decay fungi"), 
             Main(
               Table(Tbody(Tr(
-                  Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170082/fungi/birds_nest_shywg6.jpg", alt="birds nest")),
+                  Td(Div(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170082/fungi/birds_nest_shywg6.jpg", alt="birds nest")), data_tooltip="birds nest"),
                   Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170081/fungi/coral_qtnnex.jpg", alt="coral")),
                   Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170081/fungi/crust_and_parchment_ulle4s.jpg", alt="crust and parchment")),
                   Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170081/fungi/cup_and_saucer_dlst0o.jpg", alt="cup and saucer")),
@@ -19,12 +29,12 @@ def get():
               P('by Gary Emberger', align="center"),
               Table(Tbody(Tr(
                   Td(Div(
-                     A("Introduction", href="/introduction"), " | ", A("Shape Key", href="/shape-key"), " | ", A("Species List", href="/species-list"),
+                     A("Introduction", data_tooltip="Introduction", href="/introduction"), " | ", A("Shape Key", href="/shape-key"), " | ", A("Species List", href="/species-list"),
                      align="center")),
                   ),),width="100%", border="0",),
               Table(Tbody(Tr(
-                  Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170080/fungi/jelly_su2tey.jpg", alt="jelly")),
-                  Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170079/fungi/poroid_cxzog7.jpg", alt="poroid")),
+                  Td(Div(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170080/fungi/jelly_su2tey.jpg", alt="jelly")), data_tooltip="jelly"),
+                  Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170079/fungi/poroid_cxzog7.jpg", alt="poroid", data_tooltip="poroid")),
                   Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170079/fungi/puffball_wfzekp.jpg", alt="puffball")),
                   Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170079/fungi/spines_and_teeth_ef49yh.jpg", alt="spines and teeth")),
                   Td(Img(src="https://res.cloudinary.com/dptfyzzyl/image/upload/v1731170079/fungi/stinkhorn_m1iinf.jpg", alt="stinkhorn")),
@@ -47,13 +57,47 @@ def get():
             Td(Div(
                 A("Home", href="/"), " | ", A("Shape Key", href="/shape-key"),
                 align="center")),
-            ),),width="100%", border="0",),
+            ),),width="100%", height="auto", border="0",),
         cls="container")
   )
 
 @app.route("/shape-key")
 def get():
-  return
+  return (
+     Main(
+        H1("Key to Shapes", align="center"),
+        P("To begin keying, compare the specimen at hand to the illustrated and described shape categories  below. After determining the best fit, go to the dichotomous key for that category  to continue the identification process. Bear in mind that only one picture is used here to illustrate an entire group. Pay as much attention to the description as to the single image. Once you are more familiar with the shape categories, you can go directly to the keys via the menu below.", align="center"),
+        Table(Tbody(
+           Tr(
+            Td(Div(
+                A("Bird's nest", href=""),
+                " | ", 
+                A("Club, coral, leafy", href="/"),
+                " | ", 
+                A("Crust and parchment", href="/"),
+                " | ", 
+                A("Cup and saucher", href="/"),
+                " | ", 
+                A("Gilled", href="/"),
+                align="center")),
+            ),
+           Tr(
+            Td(Div(
+                A("Jelly", href=""),
+                " | ", 
+                A("Poroid", href="/"),
+                " | ", 
+                A("Puffball and cushion", href="/"),
+                " | ", 
+                A("Stinkhorn", href="/"),
+                " | ", 
+                A("Teeth and spine", href="/"),
+                align="center")),
+            ),
+          ),width="100%", height="auto", border="0",),
+          home(),
+        cls="container")
+  )
 
 @app.route("/species-list")
 def get():
